578ec6f225ade29a592abdd4e9f83d0b50b0c6d7
yunjin99 <yunjin3052@naver.com>
2022-03-29 15:49:51 +0900

    정적 파일 및 미디어 파일 처리, 블로그 글에 이미지 및 파일 첨부 추가

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -120,3 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
