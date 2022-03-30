ac3761289c9aee1d4c940b06aa6bb414feafb7f3
yunjin99 <yunjin3052@naver.com>
2022-03-31 01:15:30 +0900

    base 템플릿 navbar/footer 추가, hook 메시지 추가

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    hook_msg = models.TextField(blank=True)
+
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     attached_file = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
