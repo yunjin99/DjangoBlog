28ffa03d4c7ee571d952245a05d8f2c6180bde27
yunjin99 <yunjin3052@naver.com>
2022-04-08 15:35:04 +0900

    post에 author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -12,13 +14,16 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     attached_file = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # methods
     # 기본으로 있는 __str__ 메소드를 오버라이드
     def __str__(self):
-        return f'[{self.pk}] [{self.title}]'
+        return f'[{self.pk}] [{self.title}] :: {self.author}'
 
     def get_absolute_url(self):
         return f"/blog/{self.pk}"
